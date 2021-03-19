69d1ead79b2098c07aaa5e957999d5851e49311e
Ruby Wright <21486778@student.uwa.edu.au>
2021-03-19 08:42:40 +0800

    adding analyse subhalo function

--- GasFlowTools.py
+++ GasFlowTools.py
@@ -1060,7 +1060,6 @@ def combine_catalogues(mcut,snapidxs,nvol,snapidx_delta=1):
         isnap+=1
 
     accfile_data=pd.concat(accfile_data_vols,ignore_index=True)
-    accfile_data=accfile_data.loc[mask,:]
     ngal=accfile_data.shape[0]
     iigal=0
     for igal, gal in accfile_data.iterrows():
