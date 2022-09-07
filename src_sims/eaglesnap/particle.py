dd487ddb9736c3958912746cc95253fc33bbb709
Ruby Wright <rubyjunewright@gmail.com>
2022-09-07 16:37:11 -0400

    Adding DM

--- src_sims/eaglesnap/particle.py
+++ src_sims/eaglesnap/particle.py
@@ -22,7 +22,7 @@ def read_subvol(path,ivol,nslice,ptypes=None):
     lims=get_limits(ivol,nslice,boxsize,buffer=0.1)
     if not ptypes:
         ptypes={0:['Mass','Temperature','Metallicity','StarFormationRate'],
-                1:[],
+                1:['Mass'],
                 4:['Mass','Metallicity']}
     
     snapshot=EagleSnapshot(path)
@@ -35,14 +35,16 @@ def read_subvol(path,ivol,nslice,ptypes=None):
         pdata[ptype].loc[:,'ParticleType']=ptype
 
         for field in ptypes[ptype]:
-            hexp=file[f'PartType{ptype}/{field}'].attrs['h-scale-exponent']
-            aexp=file[f'PartType{ptype}/{field}'].attrs['aexp-scale-exponent']
-            cgs=file[f'PartType{ptype}/{field}'].attrs['CGSConversionFactor']
-            pdata[ptype][field]=snapshot.read_dataset(ptype,field)*(hfac**hexp)*(afac**aexp)*cgs
-
-        if ptype==1:
-            m_dm=file['Header'].attrs['MassTable'][1]*10**10/hfac
-            pdata[1].loc[:,'Mass']=m_dm
+            if not ptype==1:
+                hexp=file[f'PartType{ptype}/{field}'].attrs['h-scale-exponent']
+                aexp=file[f'PartType{ptype}/{field}'].attrs['aexp-scale-exponent']
+                cgs=file[f'PartType{ptype}/{field}'].attrs['CGSConversionFactor']
+                pdata[ptype][field]=snapshot.read_dataset(ptype,field)*(hfac**hexp)*(afac**aexp)*cgs
+            else:
+                hexp=file[f'PartType{0}/{field}'].attrs['h-scale-exponent']
+                aexp=file[f'PartType{0}/{field}'].attrs['aexp-scale-exponent']
+                cgs=file[f'PartType{0}/{field}'].attrs['CGSConversionFactor']
+                pdata[ptype][field]=file['Header'].attrs['MassTable'][1]*(hfac**hexp)*(afac**aexp)*cgs
     snapshot.close()
 
     
@@ -54,8 +56,7 @@ def read_subvol(path,ivol,nslice,ptypes=None):
         if not field in ptypes[4]:
             pdata[4][field]=np.ones(npart_star)*10**10
         if not field in ptypes[1]:
-            if not field=='Mass':
-                pdata[1][field]=np.ones(npart_dm)*np.nan
+            pdata[1][field]=np.ones(npart_dm)*np.nan
 
     #concat all pdata into one df
     pdata=pd.concat([pdata[ptype] for ptype in pdata],ignore_index=True,)
