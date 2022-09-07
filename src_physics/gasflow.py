4fd3f6c46b294794764713b798516f383544599c
Ruby Wright <rubyjunewright@gmail.com>
2022-09-07 16:22:08 -0400

    Adding DM

--- src_physics/gasflow.py
+++ src_physics/gasflow.py
@@ -116,8 +116,8 @@ def candidates_gasflow(galaxy_snapi,galaxy_snapf,pdata_snapi,kdtree_snapi,pdata_
     galaxy_com_snapf=np.array([galaxy_snapf[f'CentreOfPotential_{x}'] for x in 'xyz'],ndmin=2)
     
     #get gasflow candidates
-    pidx_candidates_snapi=kdtree_snapi.query_ball_point(galaxy_com_snapi[0],r200)
-    pidx_candidates_snapf=kdtree_snapf.query_ball_point(galaxy_com_snapf[0],r200)
+    pidx_candidates_snapi=kdtree_snapi.query_ball_point(galaxy_com_snapi[0],3*r200)
+    pidx_candidates_snapf=kdtree_snapf.query_ball_point(galaxy_com_snapf[0],3*r200)
 
     pids_candidates_snapi=pdata_snapi.loc[pidx_candidates_snapi,'ParticleIDs'].values
     pids_candidates_snapf=pdata_snapf.loc[pidx_candidates_snapf,'ParticleIDs'].values
