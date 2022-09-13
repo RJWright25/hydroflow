08daf702591b543c997473cee90fa39c96d5b3ab
Ruby Wright <rubyjunewright@gmail.com>
2022-09-13 11:57:50 -0400

    Adding new hpc tools

--- run/tools_hpc.py
+++ run/tools_hpc.py
@@ -5,6 +5,13 @@
 
 import os
 
+def create_dir(path):
+    running_dir=''
+    for idir in path.split('/')[:-1]:
+        running_dir=running_dir+'/'+idir
+        if not os.path.exists(running_dir):
+            os.mkdir(running_dir)
+
 def submit_gasflow_jobarray(repo,arguments,memory,time,array=None):
     
     cwd=os.getcwd()
@@ -111,9 +118,74 @@ def submit_gasflow_function(repo,function,arguments,memory,time):
     jobfile.close()
     os.system(f"sbatch {jobscriptfilepath}")
 
-def create_dir(path):
-    running_dir=''
-    for idir in path.split('/')[:-1]:
-        running_dir=running_dir+'/'+idir
-        if not os.path.exists(running_dir):
-            os.mkdir(running_dir)
+
+
+def submit_serial_job(func,memory,time,arguments={},partition=None,repo=None):
+
+    cwd=os.getcwd()
+    run=cwd.split('/')[-1]
+
+    funcname=func.split('.')[-1]
+    funcloc=func.split('.')[:-1]
+    funclocstr=''
+    for ifuncloc in funcloc:
+        funclocstr+=ifuncloc+'.'
+    funclocstr=funclocstr[:-1]
+    print(funclocstr)
+
+    jobname=f'{run}_{funcloc}'
+
+    if not os.path.exists('jobs'):
+        os.mkdir('jobs')
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    
+    runscriptfilepath=f'{cwd}/jobs/{jobname}-run.py'
+    jobscriptfilepath=f'{cwd}/jobs/{jobname}-submit.slurm'
+    if os.path.exists(runscriptfilepath):
+        os.remove(runscriptfilepath)
+    if os.path.exists(jobscriptfilepath):
+        os.remove(jobscriptfilepath)
+
+    argumentstring=''
+    for arg in arguments:
+        if type(arguments[arg])==str:
+            argumentstring+=f"{arg}='{arguments[arg]}',"
+        else:
+            argumentstring+=f"{arg}={arguments[arg]},
+
+    with open(runscriptfilepath,"w") as runfile:
+        runfile.writelines(f"import warnings\n")
+        runfile.writelines(f"warnings.filterwarnings('ignore')\n")
+        runfile.writelines(f"import sys\n")
+        runfile.writelines(f"sys.path.append({repo})\n")
+        runfile.writelines(f"from {funclocstr} import {func}\n")
+        runfile.writelines(f"{func}({argumentstring})")
+
+    runfile.close()
+
+    with open(jobscriptfilepath,"w") as jobfile:
+        jobfile.writelines(f"#!/bin/sh\n")
+        jobfile.writelines(f"#SBATCH --job-name={jobname}\n")
+        jobfile.writelines(f"#SBATCH --nodes=1\n")
+        jobfile.writelines(f"#SBATCH --ntasks-per-node=1\n")
+        jobfile.writelines(f"#SBATCH --mem={memory}GB\n")
+        jobfile.writelines(f"#SBATCH --time={time}\n")
+        jobfile.writelines(f"#SBATCH --output=jobs/{jobname}.out\n")
+        jobfile.writelines(f" \n")
+        jobfile.writelines(f"OMPI_MCA_mpi_warn_on_fork=0\n")
+        jobfile.writelines(f"export OMPI_MCA_mpi_warn_on_fork\n")
+        jobfile.writelines(f"echo JOB START TIME\n")
+        jobfile.writelines(f"date\n")
+        jobfile.writelines(f"echo CPU DETAILS\n")
+        jobfile.writelines(f"lscpu\n")
+        jobfile.writelines(f"python {runscriptfilepath} \n")
+        jobfile.writelines(f"echo JOB END TIME\n")
+        jobfile.writelines(f"date\n")
+    jobfile.close()
+
+    if partition:
+        os.system(f"sbatch {jobscriptfilepath} --p {partition} ")
+    else:
+        os.system(f"sbatch {jobscriptfilepath}")
