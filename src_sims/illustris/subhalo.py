ee91a63c692194bc05e41bd419fb283279f82dc6
Ruby Wright <rubyjunewright@gmail.com>
2022-09-16 15:34:51 -0400

    Adding TNG halo catalogs

--- src_sims/illustris/subhalo.py
+++ src_sims/illustris/subhalo.py
@@ -125,7 +125,7 @@ def read_subcat(basepath,snapnums=None):
         subcat=pd.concat(subhalo_dfs)
     else:
         subcat=subhalo_dfs[0]
-    subcat.sort_values(by='SnapNum',ascending=False,inplace=True)
+    subcat.sort_values(by=['SnapNum','Mass'],ascending=[False,False],inplace=True)
     subcat.reset_index(inplace=True,drop=True)
 
     outname=f'catalogues/catalogue_subhalo_{str(int(snapnums[0])).zfill(3)}_to_{str(int(snapnums[-1])).zfill(3)}.hdf5'
