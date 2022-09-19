4f2bc232c3cca5bc43bc6d014a7e3887fbbe1355
Ruby Wright <rubyjunewright@gmail.com>
2022-09-19 15:38:31 -0400

    Testing illustris pdata

--- src_sims/illustris/particle.py
+++ src_sims/illustris/particle.py
@@ -111,6 +111,7 @@ def read_subvol(path,ivol,nslice):
         # print(f'Matched tracers for ifile {ifile+1}/{numfiles} in {time.time()-t0:.3f} sec ({np.nanmean(tracer_match_2)*100:.2f}% matched, {np.nanmean(tracer_match_1)*100:.2f}% of the tracers in this file were in the desired ivol {ivol+1}/{nslice**3})')
 
         pdata[ifile]=pd.concat([pdata[ifile][ptype] for ptype in pdata[ifile]])
+        print(pdata[ifile])
 
         print(list(pdata[ifile].keys()))
         pdata[ifile].sort_values(by="ParticleIDs",inplace=True)
