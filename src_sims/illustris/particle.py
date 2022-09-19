a89980d9d404f1b498d84dd1697bb466531a38de
Ruby Wright <rubyjunewright@gmail.com>
2022-09-19 14:59:04 -0400

    Testing illustris pdata

--- src_sims/illustris/particle.py
+++ src_sims/illustris/particle.py
@@ -83,7 +83,6 @@ def read_subvol(path,ivol,nslice):
         ################# tracers #################
         print('Loading tracers')
         t0=time.time()
-        print(list(pdata_ifile['PartType3'].keys()))
         pdata_tracers_ifile=pd.DataFrame(np.column_stack([pdata_ifile[f'PartType3']['ParentID'][:],pdata_ifile[f'PartType3']['TracerID'][:]]),columns=['ParentID','TracerID'])
         pdata_tracers_ifile.sort_values(by='ParentID',inplace=True);pdata_tracers_ifile.reset_index(inplace=True,drop=True)
 
@@ -103,11 +102,16 @@ def read_subvol(path,ivol,nslice):
 
         expected_idx_of_tracer_in_pdata=expected_idx_of_tracer_in_pdata[tracer_match_1]
         tracer_match_2=pdata_tracer_parentIDs_invol==pdata_ifile_baryons_IDs[(expected_idx_of_tracer_in_pdata,)]
-        # print(np.nanmean)
+
+        parent_data=pdata_ifile_baryons.loc[expected_idx_of_tracer_in_pdata,:]
+        parent_data['ParentID']=pdata_tracer_parentIDs_invol
+
+        print(parent_data.loc[:,['ParticleIDs','ParentID']])
+        print(np.nanmean(parent_data['ParticleIDs'].values==parent_data['ParentID'].values))
 
         tf=time.time()
 
-        print(f'Matched tracers for ifile {ifile+1}in {tf-t0:.3f} sec({np.nanmean(tracer_match_2)*100:.2f}% matched, {np.nanmean(tracer_match_1)*100:.2f}% of tracers in ivol {ivol+1}/{nslice**3})')
+        print(f'Matched tracers for ifile {ifile+1}in {tf-t0:.3f} sec ({np.nanmean(tracer_match_2)*100:.2f}% matched, {np.nanmean(tracer_match_1)*100:.2f}% of tracers in ivol {ivol+1}/{nslice**3})')
         pdata_ifile.close()
 
 
