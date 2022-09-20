a246a6421e67e4d07ff0753f15757a7bd9239c3d
Ruby Wright <rubyjunewright@gmail.com>
2022-09-20 12:44:33 -0400

    Testing illustris pdata

--- src_physics/gasflow.py
+++ src_physics/gasflow.py
@@ -130,7 +130,9 @@ def candidates_gasflow(galaxy_snapi,galaxy_snapf,pdata_snapi,kdtree_snapi,pdata_
         pdata_candidates_idx_snapi=np.searchsorted(pdata_snapi['ParticleIDs'].values,pid_allcandidates)
         pdata_candidates_idx_snapf=np.searchsorted(pdata_snapf['ParticleIDs'].values,pid_allcandidates)
         print(pdata_snapi)
-
+        print(np.nanmax(pdata_candidates_idx_snapi))
+        print(pdata_snapf)
+        print(np.nanmax(pdata_candidates_idx_snapf))
         pdata_candidates_snapi=pdata_snapi.iloc[pdata_candidates_idx_snapi,:]
         pdata_candidates_snapf=pdata_snapf.iloc[pdata_candidates_idx_snapf,:]
 
