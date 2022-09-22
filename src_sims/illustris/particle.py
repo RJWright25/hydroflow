e8fe7ee119a59b529147144df6c10d0666b710ce
Ruby Wright <rubyjunewright@gmail.com>
2022-09-22 10:35:07 -0400

    Reverting to old version for tracer testing

--- src_sims/illustris/particle.py
+++ src_sims/illustris/particle.py
@@ -12,7 +12,7 @@ import illustris_python
 
 
 ##### READ PARTICLE DATA
-def read_subvol(path,ivol,nslice):
+def read_subvol(path,ivol,nslice,nchunks=None):
 
     pdata_file=h5py.File(path,'r')
     boxsize=pdata_file['Header'].attrs['BoxSize']
