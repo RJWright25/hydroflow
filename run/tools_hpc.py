d905784084c3a51f0581c6d7f7bd1382d8b21e5b
Ruby Wright <rubyjunewright@gmail.com>
2022-10-02 08:57:22 -0400

    Fixing TNG & SIMBA subhalo catalogue routines

--- run/tools_hpc.py
+++ run/tools_hpc.py
@@ -102,8 +102,12 @@ def submit_gasflow_disBatch(repo,arguments,memory,time,partition=None,nodetype=N
     taskfile.close()
 
     with open(submitscriptfilepath,"w") as submitfile:
-        submitfile.writelines(f"cd {disbatch_dir}\n")        
-        submitfile.writelines(f"sbatch --time {time} -n {num} --partition {partition} -C {nodetype} --mem-per-cpu {memory}GB --output {jobfolder}{jobname}.out --job-name {jobname} disBatch {jobscriptfilepath}\n")
+        submitfile.writelines(f"cd {disbatch_dir}\n")
+        if nodetype:        
+            submitfile.writelines(f"sbatch --time {time} -n {num} --partition {partition} -C {nodetype} --mem-per-cpu {memory}GB --output {jobfolder}{jobname}.out --job-name {jobname} disBatch {jobscriptfilepath}\n")
+        else:
+            submitfile.writelines(f"sbatch --time {time} -n {num} --partition {partition} --mem-per-cpu {memory}GB --output {jobfolder}{jobname}.out --job-name {jobname} disBatch {jobscriptfilepath}\n")
+
         submitfile.writelines(f"cd {cwd}\n")
 
     submitfile.close()
