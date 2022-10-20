be4ee45fa26afc272efc5697544e9683300fa439
Ruby Wright <rubyjunewright@gmail.com>
2022-10-20 15:46:44 -0400

    Updating galaxy profiling, velocity cuts and radial selections

--- run/execute.py
+++ run/execute.py
@@ -274,8 +274,8 @@ if numgal:
                             galaxy_output.loc[0,f'{str(int(rad)).zfill(3)}ckpc_gas-'+key]=np.nan
                             
                 logging.info(f'30ckpc outflow outputs:')
-                logging.info(f"Lagrangian: {galaxy_output['050ckpc_gas-000kmps_outflow-m'].values[0]:.2e} Msun/Gyr")
-                logging.info(f"Eulerian: {galaxy_output['050ckpc_gas-000kmps_outflowflux-m'].values[0]:.2e} Msun/Gyr")
+                logging.info(f"Lagrangian: {galaxy_output['1p00r200_gas-000kmps_outflow-m'].values[0]:.2e} Msun/Gyr")
+                logging.info(f"Eulerian: {galaxy_output['1p00r200_gas-000kmps_outflowflux-m'].values[0]:.2e} Msun/Gyr")
 
                             
                 ### user def
