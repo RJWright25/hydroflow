e941ecebdd026f38effa2a17fab45221d715ce4d
Ruby Wright <rubyjunewright@gmail.com>
2022-11-24 16:05:48 +0800

    Adding analysis of feedback processes

--- src_physics/gasflow.py
+++ src_physics/gasflow.py
@@ -345,6 +345,7 @@ def candidates_gasflow(galaxy_snapi,galaxy_snapf,pdata_snapi,kdtree_snapi,pdata_
         pdata_candidates_snapi=pdata_snapi.iloc[pdata_candidates_idx_snapi,:]
         pdata_candidates_snapf=pdata_snapf.iloc[pdata_candidates_idx_snapf,:]
     except:
+        raise
         failed=True
 
     numcdt_snapi=pdata_candidates_snapi.shape[0]
