d1a2b80479eaf54a3d5a32d8f2d7beb6b1d91b79
Ruby Wright <00093568@dep79487.uniwa.uwa.edu.au>
2024-11-03 01:10:09 +0800

    Adding coordinate and temperature check in execute script

--- run/execute.py
+++ run/execute.py
@@ -179,8 +179,8 @@ if numgal:
 
     logging.info(f'Coordinate minima: x - {pdata_subvol["Coordinates_x"].min():.2f}, y - {pdata_subvol["Coordinates_y"].min():.2f}, z - {pdata_subvol["Coordinates_z"].min():.2f} [runtime {time.time()-t1:.3f} sec]')
     logging.info(f'Coordinate maxima: x - {pdata_subvol["Coordinates_x"].max():.2f}, y - {pdata_subvol["Coordinates_y"].max():.2f}, z - {pdata_subvol["Coordinates_z"].max():.2f} [runtime {time.time()-t1:.3f} sec]')
-    logging.info(f'Temperature 1st percentile: {np.nanpercentile(pdata_subvol["Temperature"],1):.2e} K')
-    logging.info(f'Temperature 99th percentile: {np.nanpercentile(pdata_subvol["Temperature"],99):.2e} K')
+    logging.info(f'Temperature min: {np.nanmin(pdata_subvol["Temperature"]):.2e} K')
+    logging.info(f'Temperature max: {np.nanmax(pdata_subvol["Temperature"]):.2e} K')
     logging.info(f'Temperature mean: {pdata_subvol["Temperature"].mean():.2e}')
     
     # If dumping pdata and mfracs are in pdata, add to pdata_fields
