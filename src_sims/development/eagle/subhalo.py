8b2529b1733dec3a44c89f1bf5e3f55b8c65bee7
Ruby Wright <00093568@dep79487.uniwa.uwa.edu.au>
2024-11-02 10:25:47 +0800

    Adding EAGLE SQL & particle data support

--- src_sims/development/eagle/subhalo.py
+++ src_sims/development/eagle/subhalo.py
@@ -81,7 +81,7 @@ def extract_subhaloes(simname='RefL0100N1504',snapnums=[],uname=None,pw=None,mcu
                 Subhalo.GalaxyID = Sizes.GalaxyID and \
                 Subhalo.GroupID = FOF.GroupID and\
                 Subhalo.SnapNum = FOF.SnapNum and\
-                Subhalo.Mass>={10**mcut:.1f} \
+                Subhalo.Mass>=1e{mcut:.0f} \
               ORDER BY \
                 Subhalo.SnapNum desc, \
                 Subhalo.Mass desc \
