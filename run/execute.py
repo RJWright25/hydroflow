41d77f9b779237f26fff3eb5025da044160d3d2e
Ruby Wright <00093568@dep79487.uniwa.uwa.edu.au>
2024-11-03 01:02:04 +0800

    Adding coordinate and temperature check in execute script

--- run/execute.py
+++ run/execute.py
@@ -179,7 +179,10 @@ if numgal:
 
     logging.info(f'Coordinate minima: x - {pdata_subvol["Coordinates_x"].min():.2f}, y - {pdata_subvol["Coordinates_y"].min():.2f}, z - {pdata_subvol["Coordinates_z"].min():.2f} [runtime {time.time()-t1:.3f} sec]')
     logging.info(f'Coordinate maxima: x - {pdata_subvol["Coordinates_x"].max():.2f}, y - {pdata_subvol["Coordinates_y"].max():.2f}, z - {pdata_subvol["Coordinates_z"].max():.2f} [runtime {time.time()-t1:.3f} sec]')
-
+    logging.info(f'Temperature minima: {pdata_subvol["Temperature"].min():.2e}')
+    logging.info(f'Temperature maxima: {pdata_subvol["Temperature"].max():.2e}')
+    logging.info(f'Temperature mean: {pdata_subvol["Temperature"].mean():.2e}')
+    
     # If dumping pdata and mfracs are in pdata, add to pdata_fields
     if dump:
         if 'mfrac_HI' in pdata_subvol.columns:
