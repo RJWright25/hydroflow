bf88a31c491a58582e188e3f3bd549794907abc0
Ruby Wright <00093568@dep79487.uniwa.uwa.edu.au>
2024-11-03 00:38:40 +0800

    Adding SIMBA catalogue load

--- run/execute.py
+++ run/execute.py
@@ -220,7 +220,7 @@ if numgal:
             t1_c=time.time()
             pdata_candidates=retrieve_galaxy_candidates(galaxy,pdata_subvol,kdtree_subvol,maxrad=maxrad)
             t2_c=time.time()
-            logging.info(f"Candidates: {t2_c-t1_c:.3f} sec (n={pdata_candidates.shape[0]})")
+            logging.info(f"Candidates: {t2_c-t1_c:.3f} sec (n = {pdata_candidates.shape[0]}, m = {np.nansum(pdata_candidates['Masses'].values):1e})")
 
             # Dump a subset of the particle data if requested
             if dump:
