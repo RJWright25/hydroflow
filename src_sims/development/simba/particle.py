3a07e4af8f184edacec41f75c0f40bacee2db205
Ruby Wright <00093568@dep79487.uniwa.uwa.edu.au>
2024-11-06 21:36:18 +0800

    Adding neutral hydrogen partitioning with Rahmati (2013) and Blitz & Rosolowsky (2006)

--- src_sims/development/simba/particle.py
+++ src_sims/development/simba/particle.py
@@ -140,14 +140,14 @@ def read_subvol(path,ivol,nslice,metadata,logfile=None,verbose=False):
     # Add hydrogen partitions into HI, H2, HII from Rahmati (2013) and Blitz & Rosolowsky (2006)
     logging.info(f"Adding hydrogen partitioning...")
     gas=pdata['ParticleType'].values==0
-    fHI,fHII,fH2=partition_neutral_gas(pdata,redshift=zval,sfonly=True)
+    fHI,fH2,fHII=partition_neutral_gas(pdata,redshift=zval,sfonly=True)
     logging.info(f"Minima: fHI: {np.nanmin(fHI)}, fHII: {np.nanmin(fHII)}, fH2: {np.nanmin(fH2)}]")
     logging.info(f"Maxima: fHI: {np.nanmax(fHI)}, fHII: {np.nanmax(fHII)}, fH2: {np.nanmax(fH2)}]")
     pdata.loc[:,['mfrac_HI','mfrac_HII','mfrac_H2']]=np.nan
     pdata.loc[gas,'mfrac_HI']=fHI
     pdata.loc[gas,'mfrac_HII']=fHII
     pdata.loc[gas,'mfrac_H2']=fH2
-    
+
     # Create a spatial KDTree for the particle data
     pdata_kdtree=cKDTree(pdata.loc[:,[f'Coordinates_{x}'for x in 'xyz']].values)
     
