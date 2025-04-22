004a4baae0448893269e664516f9ac42f89f5e61
Ruby <ruby.wright@uwa.edu.au>
2025-04-22 15:28:57 +0800

    Fixing particle dump issue

--- run/execute.py
+++ run/execute.py
@@ -235,7 +235,7 @@ if numgal:
             group=str(int(galaxy[galid_key]))
             data=pdata_candidates.loc[pdata_candidates['ParticleType'].values==0,pdata_fields]
             columns=list(subcat_selection.columns)
-            for column in galaxy_output.columns:
+            for column in list(galaxy_output.keys()):
                 if '0p10r200' in column or '1p00r200' in column or '030pkpc' in column:
                     columns.append(column)
             metadata_dump={key:galaxy_output[key] for key in columns}
