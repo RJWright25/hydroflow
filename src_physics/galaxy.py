17d40a7ef4b05cf36b0cde2138d16b5b5b12d63b
Ruby <ruby.wright@uwa.edu.au>
2025-04-25 00:36:17 +0800

    Fixing z height calculations

--- src_physics/galaxy.py
+++ src_physics/galaxy.py
@@ -412,9 +412,10 @@ def analyse_galaxy(galaxy,pdata_candidates,metadata,
 						for iv,vminstr in enumerate(vminstrs):
 							galaxy_output[f'{rshell_str}_zslab{drfac_str}-gas_'+Tstr+f'-mdot_tot_outflow_{vkey}_{vminstr}']=gas_flow_rates[2+iv]
 						
-						print('Temperature mask:',Tstr)
-						print(f'Gas outflow rates regular: {galaxy_output[f"{rshell_str}_shell{drfac_str}_minax-gas_"+Tstr+"-mdot_tot_outflow_vbdef_vc000kmps"]} Msun/yr')
-						print(f'Gas outflow rates z slab: {galaxy_output[f"{rshell_str}_zslab{drfac_str}-gas_"+Tstr+"-mdot_tot_outflow_"+vkey+"_vc000kmps"]} Msun/yr')
+						if 'all' in Tstr:
+							print(rshell_str)
+							print(f'Gas outflow rates regular: {galaxy_output[f"{rshell_str}_shell{drfac_str}_minax-gas_"+Tstr+"-mdot_tot_outflow_vbdef_vc000kmps"]} Msun/yr')
+							print(f'Gas outflow rates z slab: {galaxy_output[f"{rshell_str}_zslab{drfac_str}-gas_"+Tstr+"-mdot_tot_outflow_"+vkey+"_vc000kmps"]} Msun/yr')
 
 						# Calculate the flow rates for the gas by species
 						for spec in specmass.keys():
