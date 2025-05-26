ac5c032ac08c03a6cecf5c5215de4271abf6ad45
Ruby <ruby.wright@uwa.edu.au>
2025-05-26 17:29:15 +0800

    Adding velocity-based angle in addition to position-based

--- run/defaults.py
+++ run/defaults.py
@@ -2,9 +2,9 @@
 r200_shells=[0.05,0.1,0.2,0.25,0.3,0.5,0.75,1,1.5,2,2.5,3] # Shells as fraction of r200
 rstar_shells=[0.5,1,2,4] # Shells as fraction of stellar half mass radius
 kpc_shells=[1,2,5,10,15,20,25,30,50,75,100] # Shells in pkpc 
-zslab_radii={'rmx2reff':'2r_half','rmx10pkpc':10,'rmxzheight':1}
+zslab_radii={'rmx10pkpc':10,'rmxzheight':1}
 Tbins={'cold':[0,1e3],'cool':[1e3,1e5],'warm':[1e5,1e7],'hot':[1e7,1e15]} # Temperature bins for inflow/outflow calculations
-theta_bins={'minax':[30,90],'majax':[0,30],'full':[0,90]} # Angular bins for inflow/outflow calculations
+theta_bins={'minax':[30,90],'full':[0,90]} # Angular bins for inflow/outflow calculations
 vcuts={'vc0p25vmx':'0.25Vmax','vc1p00vmx':'1.00Vmax','vc050kmps':50,'vc250kmps':250}
 drfacs=[0.1] # Fraction of shell radius to use for shell thickness (r-dr/2 - r+dr/2)
 
@@ -18,7 +18,8 @@ pdata_fields=['Masses',
               'Relative_vx_pec',
               'Relative_vy_pec',
               'Relative_vz_pec',
-              'Relative_theta',
+              'Relative_theta_pos',
+              'Relative_theta_vel',
               'Temperature',
               'Density',
               'Metallicity']
