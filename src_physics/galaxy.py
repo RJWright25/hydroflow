7fa9156d9f73808761a6e7f5e7d8331d53722419
Ruby <ruby.wright@uwa.edu.au>
2025-06-11 12:13:45 +0800

    Adding vbernoulli support

--- src_physics/galaxy.py
+++ src_physics/galaxy.py
@@ -238,7 +238,7 @@ def analyse_galaxy(galaxy,pdata_candidates,metadata,
 	mu=estimate_mu(x_H=ionised_frac_H,T=temp,y=0.08) #Estimate the mean molecular weight based on the ionised fraction and temperature
 	cs=0.129*np.sqrt(temp/mu);gamma=5/3 #ideal gas
 	vb_toinf=np.sqrt(2*(potential_infinity - potential_profile) - 2*cs**2/(gamma-1) )
-	vb_to3r=np.sqrt(2*(potential_atxrrel[gas] - potential_profile) - 2*cs**2/(gamma-1)  )
+	vb_to3r=np.sqrt(2*(potential_atxrrel - potential_profile) - 2*cs**2/(gamma-1)  )
 	vmins.append(vb_toinf);vminstrs.append('bntoinf')
 	vmins.append(vb_to3r); vminstrs.append('bnto3r')
 
