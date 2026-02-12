ca49af3d6c6525e15f9741d4e130dd2eec11f313
Ruby <ruby.wright@uwa.edu.au>
2026-02-12 18:41:03 +0800

    Editing COM calculation

--- src_physics/galaxy.py
+++ src_physics/galaxy.py
@@ -141,7 +141,7 @@ def retrieve_galaxy_candidates(galaxy, pdata_subvol, kdtree_subvol, maxrad=None,
 
     print(f'COM ref: {com_ref} Mpc')
     # Recenter within 0.3R200, 0.1R200
-    for rfac in [0.5, 0.3, 0.1]:
+    for rfac in [0.3, 0.1]:
         # mask in physical Mpc (scale is pkpc)
         mask = (radii_relative/galaxy['Group_R_Crit200']) < rfac
 
