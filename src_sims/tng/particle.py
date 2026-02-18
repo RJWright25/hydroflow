88ac4d3d9312ca27ca23d96f28ace36566a0c0a1
Ruby <ruby.wright@uwa.edu.au>
2026-02-18 14:25:07 +0800

    Changing BR06 to not only SF particles

--- src_sims/tng/particle.py
+++ src_sims/tng/particle.py
@@ -177,7 +177,7 @@ def read_subvol(path,ivol,nslice,metadata,logfile=None,verbose=False):
     # Add hydrogen partitions into HI, H2, HII from Rahmati (2013) and Blitz & Rosolowsky (2006)
     logging.info(f"Adding hydrogen partitioning...")
     gas=pdata['ParticleType'].values==0
-    fHI,fH2,fHII=partition_neutral_gas(pdata,redshift=zval,sfonly=True)
+    fHI,fH2,fHII=partition_neutral_gas(pdata,redshift=zval,sfonly=False)
     logging.info(f"Minima: fHI: {np.nanmin(fHI)}, fHII: {np.nanmin(fHII)}, fH2: {np.nanmin(fH2)}]")
     logging.info(f"Maxima: fHI: {np.nanmax(fHI)}, fHII: {np.nanmax(fHII)}, fH2: {np.nanmax(fH2)}]")
     pdata.loc[:,['mfrac_HI_BR06','mfrac_H2_BR06']]=np.nan
