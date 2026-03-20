4fb31ce1b71a0970fe5f3e4443366f806f2ad915
Ruby <ruby.wright@uwa.edu.au>
2026-03-20 12:03:34 +0800

    Updating outdated  default paramfile

--- run/default_parameters.py
+++ run/default_parameters.py
@@ -6,8 +6,8 @@ zslab_radii={'rmx05pkpc':5,'rmx10pkpc':10,'rmxzheight':1,'rmx2reff':2}
 Tbins={'cold':[0,1e3],'cool':[1e3,1e5],'warm':[1e5,1e7],'hot':[1e7,1e15]} # Temperature bins for inflow/outflow calculations
 theta_bins={'minax':[30,90],'full':[0,90]} # Angular bins for inflow/outflow calculations
 vcuts={'vc0p25vmx':'0.25Vmax','vc1p00vmx':'1.00Vmax','vc050kmps':50,'vc250kmps':250}
-drfacs=[0.05,0.1,0.2,0.4] # Fraction of shell radius to use for shell thickness (r-dr/2 - r+dr/2)
-dzfacs=[0.2,0.4,0.8] # Fraction of slab height to use for slab thickness (z-dz/2 - z+dz/2)
+drfacs=[0.1] # Fraction of shell radius to use for shell thickness (r-dr/2 - r+dr/2)
+dzfacs=[0.4] # Fraction of slab height to use for slab thickness (z-dz/2 - z+dz/2)
 
 # Particle data fields to dump if requested
 pdata_fields=['Masses',
